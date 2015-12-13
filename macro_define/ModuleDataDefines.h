b04ebd0131e8e16a3f5726c006cb12179fe597d6
zengfancy <zengfancy@126.com>
2015-12-13 18:20:52 +0800

    license added

--- macro_define/ModuleDataDefines.h
+++ macro_define/ModuleDataDefines.h
@@ -1,3 +1,24 @@
+// Copyright (C) 2014, 2015 Fanxi Zeng
+// Contact: zengfancy@126.com 
+// 
+// This file is part of xzerver.
+// 
+// xzerver is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// xzerver is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with xzerver. If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef __MODULE_DATA_DEFINES_H__
+#define __MODULE_DATA_DEFINES_H__
 
 #define BEGIN_MODULE_DATA(KaraModuleData, KaraModule) \
 class KaraModuleData { \
@@ -37,3 +58,4 @@ private: \
 #define END_MODULE_DATA() \
 };
 
+#endif
