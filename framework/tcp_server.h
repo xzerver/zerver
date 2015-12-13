b3006805581b4cb0018c1ff7c0b344be06c3ab00
zengfancy <zengfancy@126.com>
2015-12-13 18:22:15 +0800

    license added

--- framework/tcp_server.h
+++ framework/tcp_server.h
@@ -1,3 +1,21 @@
+// Copyright (C) 2014, 2015 Fanxi Zeng
+// Contact: zengfancy@126.com 
+// 
+// This file is part of xzerver.
+// 
+// xzerver is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// xzerver is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with xzerver. If not, see <http://www.gnu.org/licenses/>.
+//
 #ifndef __ZERVER_TCP_SERVER_H__
 #define __ZERVER_TCP_SERVER_H__
 
